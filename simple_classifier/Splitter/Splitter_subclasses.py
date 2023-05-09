781ffda95f6b8724c3331d7252a5d50dd1029867
gunjan dayani <dgh007786007@gmail.com>
2023-05-08 22:53:13 -0400

    Sphinx Docstring conversion

--- simple_classifier/Splitter/Splitter_subclasses.py
+++ simple_classifier/Splitter/Splitter_subclasses.py
@@ -5,24 +5,17 @@ from simple_classifier.Splitter.Splitter import Splitter
 
 @Splitter.register("percentage_split")
 class PercentageSplitter(Splitter):
-    """
-    This class will cover split by percentage, as well as split by Random state. If you don't want to use random state
+    """This class will cover split by percentage, as well as split by Random state. If you don't want to use random state
     set random_state = None. This will apply for every other subclasses also.
 
-    Returns:
-    --------
-    X_train : array-like or sparse matrix of shape (n_train_samples, n_features)
+    :return: X_train : array-like or sparse matrix of shape (n_train_samples, n_features)
     The training input data.
-
     X_test : array-like or sparse matrix of shape (n_test_samples, n_features)
     The testing input data.
-
     y_train : array-like of shape (n_train_samples,)
     The training target labels.
-
     y_test : array-like of shape (n_test_samples,)
     The testing target labels.
-
     """
 
     def split_data(self, X, y):
@@ -43,8 +36,7 @@ class PercentageShuffleSplitter(Splitter):
 
 @Splitter.register("percentage_split_w_stratify")
 class PercentageStratifiedSplitter(Splitter):
-    """
-    If stratify is not None, the function uses the target variable y to group the shuffled data by class. It then
+    """If stratify is not None, the function uses the target variable y to group the shuffled data by class. It then
     calculates the proportion of each class in the original dataset and ensures that the same proportion of each
     class is present in both the training and test sets. For example, if the original dataset has 70% of samples
     in class A and 30% in class B, the training and test sets will also have the same 70/30 split of classes.
@@ -58,18 +50,13 @@ class PercentageStratifiedSplitter(Splitter):
 
 @Splitter.register("percentage_split_w_cross_validation")
 class PercentageCrossValSplitter(Splitter):
-    """
-    Splits a dataset into a training set, a validation set, and a testing set using scikit-learn's
+    """Splits a dataset into a training set, a validation set, and a testing set using scikit-learn's
     train_test_split() function.
 
-    Returns:
-    --------
-    X_val : array-like or sparse matrix of shape (n_val_samples, n_features)
+    :return: X_val : array-like or sparse matrix of shape (n_val_samples, n_features)
     The validation input data.
-
     y_val : array-like of shape (n_val_samples,)
     The validation target labels.
-
     """
 
     def split_data(self, X, y):
